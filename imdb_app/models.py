d569ebad4f3d5f3e05bd23cf9f8237958a284ca0
Andy Mc <andymcc77@blueyonder.co.uk>
2021-06-17 16:49:21 +0100

    Chapter 22

--- imdb_app/models.py
+++ imdb_app/models.py
@@ -11,8 +11,9 @@ class StreamPlatform(models.Model):
 class WatchList(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist", default=None)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.title + ' / ' + str(self.platform)
