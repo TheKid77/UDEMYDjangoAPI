a2dde06b4bdb3746f083608c29c83bab8d19d036
Andy Mc <andymcc77@blueyonder.co.uk>
2021-06-17 14:25:08 +0100

    Chapter 20

--- imdb_app/api/serializers.py
+++ imdb_app/api/serializers.py
@@ -1,33 +1,24 @@
 from rest_framework import serializers
-from imdb_app.models import Movie, Movie
+from imdb_app.models import Movie
 
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError("Name is too short!")
+class MovieSerializer(serializers.ModelSerializer):
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers. CharField()
-    active = serializers.BooleanField()
+    len_name = serializers.SerializerMethodField()
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    def get_len_name(self, object):
+        return len(object.name)
 
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError("Name and Description should be different!")
-        return value
+        return data
 
-    # def validate_name(self, value):
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError("Name is too short!")
-    #     else:
-    #         return value
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short!")
+        else:
+            return value
