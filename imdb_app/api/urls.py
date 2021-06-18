c833ca0d6d54c32e81efec10e604260ba3a56382
Andy Mc <andymcc77@blueyonder.co.uk>
2021-06-18 14:07:25 +0100

    Chapter 28

--- imdb_app/api/urls.py
+++ imdb_app/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
 # from imdb_app.api.views import movie_list, movie_details
-from imdb_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from imdb_app.api.views import ReviewList, ReviewDetail, WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),    
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
