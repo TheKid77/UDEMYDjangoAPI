3a388c21d61eea31ed591a97c70cce02fea57697
Andy Mc <andymcc77@blueyonder.co.uk>
2021-06-23 16:17:59 +0100

    usermodel

--- imdb_app/api/views.py
+++ imdb_app/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.exceptions import ValidationError 
 from imdb_app.models import WatchList, StreamPlatform, Review
 from imdb_app.api.serializers import (WatchListSerializer, StreamPlatformSerializer, 
                                         ReviewSerializer)
@@ -26,10 +27,20 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer  
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+
+        serializer.save(watchlist=movie, review_user=review_user)
 
 # class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 #     queryset = Review.objects.all()
