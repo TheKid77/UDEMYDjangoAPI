e83e40519a5ce3dcc769dfd0f310992c274551b2
Andy Mc <andymcc77@blueyonder.co.uk>
2021-06-24 10:08:21 +0100

    login added

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('imdb_app.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('home.urls')),
 ]
