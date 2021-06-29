a40a17ff2bf1d0884b0f54c09b443cd451ae522e
Andy Mc <andymcc77@blueyonder.co.uk>
2021-06-29 12:30:57 +0100

    add swagger

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +24,22 @@ urlpatterns = [
 #   path('api-auth/', include('rest_framework.urls')),
     path('account/', include('user_app.api.urls')),
     path('', include('home.urls')),
+    # ...
+    # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
+    #   * `title` and `description` parameters are passed to `SchemaGenerator`.
+    #   * Provide view name for use with `reverse()`.
+    path('openapi', get_schema_view(
+        title="Andy's IMDB API Documentation",
+        description="API for all things …",
+        version="1.0.0",
+    ), name='openapi-schema'),
+    # ...
+        # ...
+    # Route TemplateView to serve Swagger UI template.
+    #   * Provide `extra_context` with view name of `SchemaView`.
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
+
